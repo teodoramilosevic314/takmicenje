52b05aff7422e04c9e21e83d49b2beb94c29f3ae
teodoramilosevic314 <55757467+teodoramilosevic314@users.noreply.github.com>
2021-11-07 19:38:44 +0100

    Ажурирање main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,10 +1,6 @@
 function kradja () {
     if (input.acceleration(Dimension.Strength) > 1034) {
         basic.showIcon(IconNames.Angry)
-        for (let index = 0; index < 4; index++) {
-            music.playTone(880, music.beat(BeatFraction.Whole))
-            music.playTone(988, music.beat(BeatFraction.Whole))
-        }
     } else {
         basic.clearScreen()
     }
@@ -14,18 +10,43 @@ radio.onReceivedString(function (receivedString) {
         otkljucano = 1
     } else {
         greska += 1
+        basic.showIcon(IconNames.No)
+        basic.clearScreen()
     }
 })
-let otkljucano = 0
 let sifra = ""
+let otkljucano = 0
 radio.setGroup(234)
-sifra = ":)"
 let greska = 0
 otkljucano = 0
+sifra = ":)"
+let blokiraj = 0
+basic.forever(function () {
+    if (blokiraj == 1) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            # # # # #
+            . . . . .
+            . . . . .
+            `)
+        basic.pause(5000)
+        basic.clearScreen()
+        blokiraj = 0
+    }
+})
 basic.forever(function () {
     if (otkljucano == 1) {
-        basic.showIcon(IconNames.Happy)
+        basic.showIcon(IconNames.Yes)
+        basic.pause(1000)
+        basic.clearScreen()
+        otkljucano = 0
     } else {
         kradja()
+    }
+})
+basic.forever(function () {
+    if (greska > 2) {
+        blokiraj += 1
     }
 })
